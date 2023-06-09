f3b535034fce97338ce75a908cdddebbe10f364b
tantawy997 <abdelrhman.eltantawy97@gmail.com>
2023-06-09 07:10:56 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatTabsModule } from "@angular/material/tabs"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GaugeModule } from 'angular-gauge';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from "@angular/material/select";
+import {MatIconModule} from "@angular/material/icon"
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GaugeModule.forRoot(),
+    MatTabsModule,
+    MatSelectModule,
+    MatIconModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    FormsModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
